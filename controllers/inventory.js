a61f0fa2e6cb5b3a469f6670ea79ba024a55aa07
Christopher Cortes <christopherctes@hotmail.com>
2024-03-27 21:19:58 -0300

    changing GET all and single

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -1,29 +1,3 @@
-// const dataBase = require("../models");
-// const ObjectId = require("mongodb").ObjectId;
-
-// const getAllInventory = async (req, res) => {
-//   const result = await dataBase.getDatabase().db().collection("").find();
-//   result.toArray().then((lists) => {
-//     res.setHeader("Content-Type", "application/json");
-//     res.status(200).json(lists);
-//   });
-// };
-
-// const getSingleInventory = async (req, res) => {
-//   const userId = new ObjectId(req.params.id);
-//   const result = await dataBase
-//     .getDatabase()
-//     .db()
-//     .collection("")
-//     .find({ _id: userId });
-//   result.toArray().then((lists) => {
-//     res.setHeader("Content-Type", "application/json");
-//     res.status(200).json(lists[0]);
-//   });
-// };
-
-// module.exports = { getAllInventory, getSingleInventory };
-
 const dataBase = require("../models");
 const Inventoryschema = dataBase.inventory;
 
@@ -48,7 +22,7 @@ const getAllInventory = (req, res) => {
       .catch((err) => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while retrieving inventory.",
+            err.message || "Some error occurred while retrieving product name.",
         });
       });
   } else {

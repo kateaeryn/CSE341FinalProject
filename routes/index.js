2f4dba1b0624314d37eebf9d670c0c6306f4fd09
Daviles82 <avileshouston@live.com>
2024-03-30 14:07:19 -0500

    david's week 5 oath implementation

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,20 @@ const revCont = require('../controllers/reviews');
 const accCont = require('../controllers/account');
 const { vAccount, vOrder, vReview, vInventory } = require('../middleware/validate');
 const { isAuthenticated } = require('../middleware/authenticate');
+const passport = require('passport');
+
+//login route
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+//logout route
+router.get('/logout', function (req, res, next) {
+  req.logOut(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
 
 
 router.use('/', require('./swagger'));
@@ -12,28 +26,28 @@ router.use('/', require('./swagger'));
 router.get('/', (req, res) => { res.send('Welcome to the little BIG Store') });
 
 //inventory routes
-
-router.post('/inventory', vInventory, invCont.newProduct);
-router.put('/inventory/:id', vInventory, invCont.updateProduct);
+router.get('/inventory', invCont.getAll); 
+router.post('/inventory', isAuthenticated, vInventory, invCont.newProduct);
+router.put('/inventory/:id', isAuthenticated, vInventory, invCont.updateProduct);
 
 
 //order routes
-
-router.post('/orders', vOrder, ordCont.newOrder)
-router.put('/orders/:id', vOrder, ordCont.updateOrder);
+router.get('/orders', ordCont.newOrder)
+router.post('/orders', isAuthenticated, vOrder, ordCont.newOrder)
+router.put('/orders/:id', isAuthenticated, vOrder, ordCont.updateOrder);
 
 
 //review routes
 
-router.post('/reviews', vReview, revCont.newReview);
-router.put('/reviews/:id', vReview, revCont.updateReview);
+router.post('/reviews', isAuthenticated, vReview, revCont.newReview);
+router.put('/reviews/:id', isAuthenticated, vReview, revCont.updateReview);
 
 
 
 //account routes
 
-router.post('/account', vAccount, accCont.newAccount);
-router.put('account/:id', vAccount, accCont.updateAccount);
+router.post('/account', isAuthenticated, vAccount, accCont.newAccount);
+router.put('account/:id', isAuthenticated, vAccount, accCont.updateAccount);
 
 
 
--- server.js
+++ server.js
@@ -8,13 +8,63 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 5500;
 const dataBase = require('./models');
 const session = require('express-session');
+const cors = require('cors');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
+
 
 
 /**************************
  * Middleware
  **************************/
 app.use(bodyParser.json())
-    .use('/', require('./routes'));
+    .use(session({ secret: 'secret', resave: false, saveUninitialized: true }))
+    .use(passport.initialize())
+    .use(passport.session())
+    .use('/', require('./routes'))
+    .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
+    .use(cors({ origin: '*' }))
+
+passport.use(
+new GitHubStrategy(
+    {
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+    }
+)
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
+
+  app.get('/', (req, res) => {
+    res.send(
+      req.session.user !== undefined
+        ? `Logged in as ${req.session.user.displayName}`
+        : 'Logged Out'
+    )
+  });
+  
+app.get(
+'/github/callback',
+passport.authenticate('github', {
+    failureRedirect: '/api-docs',
+    session: false
+}),
+(req, res) => {
+    req.session.user = req.user;
+    res.redirect('/');
+});
+
+
 
 /****************************
  * Error handling
